3836d5bb4f8cca5e44ea53a55aaa982bca05d756
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-02-05 22:51:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,7 @@ class Scraper
   
   def get_page 
     doc= Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry 
   end 
   
   def print_courses
@@ -22,6 +23,8 @@ class Scraper
   end
   
 end
+
+Scraper.new.get_page 
 
 
 
